7c779007363444cbd9493e80af327368104fd13e
Aranyak Ghosh <59360618+unicornlab-aranyakg@users.noreply.github.com>
2022-01-08 02:27:22 +0400

    Updated user model

--- user-profile-service/models/user.go
+++ user-profile-service/models/user.go
@@ -3,16 +3,21 @@ package models
 import (
 	"time"
 
+	uuid "github.com/satori/go.uuid"
 	"gorm.io/gorm"
 )
 
 type User struct {
-	gorm.Model
+	id          uuid.UUID `gorm:"type:uuid;primary_key;default:uuid_generate_v4()"`
 	name        string
 	email       string
 	imgUrl      string
 	country     string
 	dateOfBirth time.Time
+	lastLogin   time.Time
+	createdAt   time.Time
+	updatedAt   time.Time
+	deletedAt   gorm.DeletedAt `gorm:"index"`
 
 	follows []User `gorm:"many2many:user_follows"`
 }
