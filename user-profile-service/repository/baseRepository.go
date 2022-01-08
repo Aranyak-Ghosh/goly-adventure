85504cfd33f73fd1c0cc17d15d0d568e923ce5a1
Aranyak Ghosh <59360618+unicornlab-aranyakg@users.noreply.github.com>
2022-01-08 13:10:27 +0400

    Updated method names per convention

--- user-profile-service/repository/baseRepository.go
+++ user-profile-service/repository/baseRepository.go
@@ -3,9 +3,9 @@ package repository
 import uuid "github.com/satori/go.uuid"
 
 type BaseRepository interface {
-	getById(id uuid.UUID) (interface{}, error)
-	list(searchParam string, offset int, limit int) ([]interface{}, int, error)
-	create(model interface{}) error
-	update(model interface{}) error
-	delete(model interface{}) error
+	GetById(id uuid.UUID) (*interface{}, error)
+	List(searchParam string, offset int, limit int) ([]interface{}, int, error)
+	Create(model interface{}) error
+	Update(model interface{}) error
+	Delete(model interface{}) error
 }
