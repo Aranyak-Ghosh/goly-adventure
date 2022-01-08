b836e306c2ab8acda09a984b07309f8990589551
Aranyak Ghosh <59360618+unicornlab-aranyakg@users.noreply.github.com>
2022-01-09 00:11:35 +0400

    Dummy Driver

--- user-profile-service/main.go
+++ user-profile-service/main.go
@@ -1,1 +1,33 @@
 package main
+
+import (
+	"github.com/Aranyak-Ghosh/spotify/initializer"
+	user "github.com/Aranyak-Ghosh/spotify/models/user"
+)
+
+func main() {
+
+	dbConfig, ex := initializer.InitializeConfigs()
+
+	if ex != nil {
+		panic(ex)
+	}
+
+	db, err := initializer.InitializeDbContext(dbConfig)
+
+	if err != nil {
+		panic(err)
+	}
+
+	userRepo := user.UserDAO{}
+
+	userRepo.Initialize(db)
+
+	err = userRepo.Migrate()
+
+	if err != nil {
+		panic(err)
+	}
+
+	println("Hello, World!")
+}
