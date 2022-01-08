c215acf546c8cc913351dfe8d058a738eddd5f25
Aranyak Ghosh <59360618+unicornlab-aranyakg@users.noreply.github.com>
2022-01-08 16:24:08 +0400

    Added user repository

--- user-profile-service/models/user/userRepository.go
+++ user-profile-service/models/user/userRepository.go
@@ -4,22 +4,31 @@ import (
 	uuid "github.com/satori/go.uuid"
 )
 
-func (mw *User) GetById(id uuid.UUID) (*User, error) {
-	return nil, nil
+func (mw *UserDAO) GetById(id uuid.UUID) (*User, error){
+	result := mw.db.First(&mw.User, id)
+	
+	return &mw.User, result.Error
 }
 
-func (mw *User) List(searchParam string, offset int, limit int) ([]User, int, error)
-{
-	return nil, 0, nil
+func (mw *UserDAO) List(searchParam string, offset int, limit int) ([]User, int, error){
+	var users User[]
+	var count int32
+	query := mw.db.Where("name LIKE ?", "%"+searchParam+"%").Or("email LIKE ?","%"+searchParam+"%").Order("name DESC").Offset(offset).Limit(limit)
+	queryResult := query.Find(&users)
+	query.Count(&count)
+	return users, count, queryResult.Error
 }
 
-func (mw *User) Create(model interface{}) error{
-	return nil
+func (mw *UserDAO) Create(model *User) error{
+	result := mw.db.Create(&model);
+	return result.Error
 }
 
-func (mw *User) Update(model interface{}) error{
-	return nil
+func (mw *UserDAO) Update(model *User) error{
+	result := mw.db.Save(&model);
+	return result.Error
 }
-func (mw *User) Delete(model interface{}) error{
-	return nil
+func (mw *UserDAO) Delete(model *User) error{
+	result := mw.db.Delete(&model);
+	return result.Error
 }
