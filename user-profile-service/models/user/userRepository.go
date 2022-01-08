c894d66fc7fd995c22f1b49b6dcd45f844bfa5e7
Aranyak Ghosh <59360618+unicornlab-aranyakg@users.noreply.github.com>
2022-01-08 16:34:25 +0400

    Updates to repository

--- user-profile-service/models/user/userRepository.go
+++ user-profile-service/models/user/userRepository.go
@@ -4,31 +4,31 @@ import (
 	uuid "github.com/satori/go.uuid"
 )
 
-func (mw *UserDAO) GetById(id uuid.UUID) (*User, error){
+func (mw *UserDAO) GetById(id uuid.UUID) (*User, error) {
 	result := mw.db.First(&mw.User, id)
-	
+
 	return &mw.User, result.Error
 }
 
-func (mw *UserDAO) List(searchParam string, offset int, limit int) ([]User, int, error){
-	var users User[]
-	var count int32
-	query := mw.db.Where("name LIKE ?", "%"+searchParam+"%").Or("email LIKE ?","%"+searchParam+"%").Order("name DESC").Offset(offset).Limit(limit)
+func (mw *UserDAO) List(searchParam string, offset int, limit int) ([]User, int64, error) {
+	var users []User
+	var count *int64
+	query := mw.db.Where("name LIKE ?", "%"+searchParam+"%").Or("email LIKE ?", "%"+searchParam+"%").Order("name DESC").Offset(offset).Limit(limit)
 	queryResult := query.Find(&users)
-	query.Count(&count)
-	return users, count, queryResult.Error
+	query.Count(count)
+	return users, *count, queryResult.Error
 }
 
-func (mw *UserDAO) Create(model *User) error{
-	result := mw.db.Create(&model);
+func (mw *UserDAO) Create(model *User) error {
+	result := mw.db.Create(&model)
 	return result.Error
 }
 
-func (mw *UserDAO) Update(model *User) error{
-	result := mw.db.Save(&model);
+func (mw *UserDAO) Update(model *User) error {
+	result := mw.db.Save(&model)
 	return result.Error
 }
-func (mw *UserDAO) Delete(model *User) error{
-	result := mw.db.Delete(&model);
+func (mw *UserDAO) Delete(model *User) error {
+	result := mw.db.Delete(&model)
 	return result.Error
 }
