d3c02acd1717657c1acda52a8c096918d6f389cb
Aranyak Ghosh <59360618+unicornlab-aranyakg@users.noreply.github.com>
2022-01-09 00:11:05 +0400

    Added utility methods to repository

--- user-profile-service/models/user/userRepository.go
+++ user-profile-service/models/user/userRepository.go
@@ -2,7 +2,12 @@ package user
 
 import (
 	uuid "github.com/satori/go.uuid"
+	"gorm.io/gorm"
 )
+
+func (user *UserDAO) Initialize(db *gorm.DB) {
+	user.db = db
+}
 
 func (mw *UserDAO) GetById(id uuid.UUID) (*User, error) {
 	result := mw.db.First(&mw.User, id)
@@ -13,8 +18,8 @@ func (mw *UserDAO) GetById(id uuid.UUID) (*User, error) {
 func (mw *UserDAO) List(searchParam string, offset int, limit int) ([]User, int64, error) {
 	var users []User
 	var count *int64
-	query := mw.db.Where("name LIKE ?", "%"+searchParam+"%").Or("email LIKE ?", "%"+searchParam+"%").Order("name DESC").Offset(offset).Limit(limit)
-	queryResult := query.Find(&users)
+	query := mw.db.Where("name LIKE ?", "%"+searchParam+"%").Or("email LIKE ?", "%"+searchParam+"%").Order("name DESC")
+	queryResult := query.Offset(offset).Limit(limit).Find(&users)
 	query.Count(count)
 	return users, *count, queryResult.Error
 }
@@ -28,7 +33,34 @@ func (mw *UserDAO) Update(model *User) error {
 	result := mw.db.Save(&model)
 	return result.Error
 }
+
 func (mw *UserDAO) Delete(model *User) error {
 	result := mw.db.Delete(&model)
 	return result.Error
 }
+
+func (mw *UserDAO) Migrate() error {
+	result := mw.db.AutoMigrate(&User{})
+	return result
+}
+
+func (mw *UserDAO) Follow(user *User, follow *User) error {
+	err := mw.db.Model(&user).Association("Follows").Append(follow)
+	return err
+}
+
+func (mw *UserDAO) UnFollow(user *User, follow *User) error {
+	err := mw.db.Model(&user).Association("Follows").Delete(follow)
+	return err
+}
+
+func (mw *UserDAO) ListFollowing(user *User, offset int, limit int) ([]User, int64, error) {
+	var users []User
+	var count *int64
+	query := mw.db.Model(&user).Association("Follows")
+	queryError := query.Find(&users)
+	*count = query.Count()
+	return users, *count, queryError
+}
+
+// func (mw *UserDAO) ListFollowers(user *U)
--- user-profile-service/repository/baseRepository.go
+++ user-profile-service/repository/baseRepository.go
@@ -1,11 +1,16 @@
 package repository
 
-import uuid "github.com/satori/go.uuid"
+import (
+	uuid "github.com/satori/go.uuid"
+	"gorm.io/gorm"
+)
 
 type BaseRepository interface {
+	Initialize(db *gorm.DB)
 	GetById(id uuid.UUID) (*interface{}, error)
 	List(searchParam string, offset int, limit int) ([]interface{}, int, error)
 	Create(model interface{}) error
 	Update(model interface{}) error
 	Delete(model interface{}) error
+	Migrate() error
 }
