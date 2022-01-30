63fa5fc7009c91bc92ba74a3bf64e972364ad123
Aranyak Ghosh <59360618+unicornlab-aranyakg@users.noreply.github.com>
2022-01-30 20:00:51 +0400

    Clean slate- do over (Kinda)

--- user-profile-service/go.mod
+++ user-profile-service/go.mod
@@ -2,6 +2,8 @@ module github.com/Aranyak-Ghosh/spotify
 
 go 1.17
 
+require gopkg.in/natefinch/lumberjack.v2 v2.0.0
+
 require (
 	github.com/denisenkom/go-mssqldb v0.11.0 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
@@ -9,6 +11,7 @@ require (
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/go-playground/validator/v10 v10.10.0 // indirect
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/golang-sql/civil v0.0.0-20190719163853-cb61b32ac6fe // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/uuid v1.3.0 // indirect
--- user-profile-service/main.go
+++ user-profile-service/main.go
@@ -1,57 +1,5 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/Aranyak-Ghosh/spotify/initializer"
-	user "github.com/Aranyak-Ghosh/spotify/models/user"
-)
-
 func main() {
 
-	dbConfig, ex := initializer.InitializeConfigs()
-
-	if ex != nil {
-		panic(ex)
-	}
-
-	db, err := initializer.InitializeDbContext(dbConfig)
-
-	if err != nil {
-		panic(err)
-	}
-
-	userRepo := user.UserDAO{}
-
-	userRepo.Initialize(db)
-
-	err = userRepo.Migrate()
-
-	if err != nil {
-		panic(err)
-	} else {
-		err = userRepo.SeedData()
-
-		if err != nil {
-			panic(err)
-		} else {
-			var users []user.User
-			var count int64
-			var exc error
-			users, count, exc = userRepo.List("", 0, 10)
-
-			fmt.Printf("users %s, %s\n", users[0].ID, users[1].ID)
-			err = userRepo.Follow(&users[0], &users[1])
-			if err != nil {
-				panic(err)
-			}
-			print(users)
-			println(count)
-			if exc != nil {
-				panic(exc)
-			}
-		}
-	}
-
-	println("Hello, World!")
 }
