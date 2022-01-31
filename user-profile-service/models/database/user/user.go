9f024fd1de42377f909b10efc217f7cf4ce08eba
Aranyak Ghosh <aranyakghosh@gmail.com>
2022-01-31 04:30:24 +0400

    Added json tags to model

--- user-profile-service/models/database/user/user.go
+++ user-profile-service/models/database/user/user.go
@@ -7,18 +7,18 @@ import (
 )
 
 type User struct {
-	ID          string `gorm:"primary_key;size:36"`
-	Name        string
-	Email       string `gorm:"unique_index"`
-	ImgUrl      string
-	Country     string
-	Public      bool `gorm:"default:true"`
-	DateOfBirth time.Time
-	CreatedAt   time.Time
-	UpdatedAt   time.Time
-	DeletedAt   gorm.DeletedAt `gorm:"index"`
+	ID          string         `gorm:"primary_key;size:36" json:"id"`
+	Name        string         `json:"name"`
+	Email       string         `gorm:"unique_index" json:"email"`
+	ImgUrl      string         `json:"imgUrl"`
+	Country     string         `json:"country"`
+	Public      bool           `gorm:"default:true" json:"public"`
+	DateOfBirth time.Time      `json:"dateOfBirth"`
+	CreatedAt   time.Time      `json:"createdAt"`
+	UpdatedAt   time.Time      `json:"updatedAt"`
+	DeletedAt   gorm.DeletedAt `gorm:"index" json:"-"`
 
-	Follows []User `gorm:"many2many:user_follows"`
+	Follows []User `gorm:"many2many:user_follows" json:"follows,omitempty"`
 }
 
 type UserRepository struct {
