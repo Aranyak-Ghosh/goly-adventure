59fe5a0fac7310361ba263611f9dd18404d9ed56
Aranyak Ghosh <aranyakghosh@gmail.com>
2022-02-07 22:32:08 +0400

    Added create track method

---
+++ media-storage-service/repository/TrackRepository.go
@@ -0,0 +1,21 @@
+package repository
+
+import (
+	"github.com/Aranyak-Ghosh/spotigo/media_storage/models/entity"
+	"github.com/Aranyak-Ghosh/spotigo/media_storage/utils/db"
+
+	"github.com/neo4j/neo4j-go-driver/v4/neo4j"
+)
+
+func AddTrack(track entity.TrackEntity) neo4j.TransactionWork {
+	return func(tx neo4j.Transaction) (interface{}, error) {
+		var query, data = db.GenerateCreateQuery(entity.Track, track)
+		var result, err = tx.Run(query, data)
+		if err != nil {
+			return nil, err
+		}
+		return result.Consume()
+	}
+}
+
+func ListTracks()
